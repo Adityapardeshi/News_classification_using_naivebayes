e1472d7673dffa1f2c105679cee1961ae475eb71
Aditya Pardeshi <adityapardeshi862@gmail.com>
2023-05-15 22:10:50 +0530

    added categories page and login functionality and other work

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from model import user_model
 import requests
+import json
 
 obj = user_model()
 app = Flask(__name__)
 
+app.secret_key = "newsx"
 
 @app.route('/')
 def index():
@@ -26,6 +28,18 @@ def login():
 
 @app.route('/news')
 def home():
+    if 'email' in session:
+        print('logged in as '+session['email'])
+
+        with open('sample.json','r') as f: #get the user preferences from json file
+            data=json.load(f) 
+            for i in range(len(data["userPreference"])):
+                if data["userPreference"][i]['email'] == session['email']:
+                    print(data["userPreference"][i]['categories'])
+                    break;
+    else: 
+        return redirect('/login')
+
     top_url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=86b9cef2935244629fd8d031b95e6cc2"
     response = requests.get(top_url)
     top_articles = response.json()['articles']
@@ -36,15 +50,14 @@ def home():
     # print(articles)
     return render_template("news.html", top_articles=top_articles, trend_articles=trend_articles)
 
-@app.route('/categories')
-def categories():
-    return render_template("select_interests.html")
+@app.route('/select_interests', methods=["GET", "POST"])
+def select_interests():
+    if request.method=='POST':
+        return obj.select_interests(request.form)
+    else:
+        return render_template("select_interests.html")
 
 
-@app.route('/getdata')
-def hello_world():
-    
-    return obj.user_signup()
 
 if __name__ == '__main__':
     app.run(host= "127.0.0.1", port=6500)
--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import mysql.connector
-from flask import redirect, url_for
+from flask import redirect, url_for, session
+import json
 
 class user_model():
     def __init__(self):
@@ -15,14 +16,34 @@ class user_model():
         dob = (data['dob'])
         country = data['country']
         self.cur.execute("INSERT INTO userData (name, email, password, dob, country) VALUES (%s, %s, %s, %s, %s)", (name, email, password, dob, country))
-        return redirect(url_for('register'))
+        return redirect(url_for('select_interests'))
 
     def login(self, data):
         email = data['email']
         password = data['password']
         self.cur.execute("SELECT * FROM userData WHERE email = %s and password = %s", (email, password))
         user = self.cur.fetchone()
+
         if user:
+            session['email'] = email
             return redirect(url_for('home'))
         else :
             return "Invalid email or password"
+
+
+    def select_interests(self, data):
+
+        check = data.getlist('checkbox')
+        dictionary = {
+            "email": "adityaopardeshi@gmail.com",
+            "categories": check
+        }
+
+        with open("sample.json", 'r+') as file:
+            file_data = json.load(file)
+            file_data["userPreference"].append(dictionary)
+            file.seek(0)
+            json.dump(file_data, file, indent = 4)
+    
+        #print(check)
+        return redirect(url_for('home'))
