f125fcae347fb2aef5eb78e1bdeb46608a939995
himanshuch9422 <85391427+himanshuch9422@users.noreply.github.com>
2023-05-16 10:49:20 +0530

    Add files via upload

---
+++ NewsClassificationModel.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, render_template
+import requests
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+dataset = pd.read_csv('/Users/Himanshu/Documents/BBC News Train.csv')
+dataset['Text'] = dataset['Text'].apply(lambda x: re.sub(r'<[^>]*>', '', x)) # Remove HTML tags
+dataset['Text'] = dataset['Text'].apply(lambda x: re.sub('[^a-zA-Z0-9\s]', '', x)) # Remove special characters
+dataset['Text'] = dataset['Text'].apply(lambda x: x.lower()) # Convert to lowercase
+stop_words = set(stopwords.words('english'))
+dataset['Text'] = dataset['Text'].apply(lambda x: ' '.join([word for word in word_tokenize(x) if word not in stop_words])) # Remove stopwords
+lemmatizer = WordNetLemmatizer()
+dataset['Text'] = dataset['Text'].apply(lambda x: ' '.join([lemmatizer.lemmatize(word) for word in word_tokenize(x)])) # Lemmatize words
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(dataset['Text'])
+y = dataset['Category']
+
+
+clf = MultinomialNB()
+clf.fit(X, y)
+
+#Input as a News Headline
+text = input("Enter your News: ")
+
+
+text = re.sub(r'<[^>]*>', '', text) # Remove HTML tags
+text = re.sub('[^a-zA-Z0-9\s]', '', text) # Remove special characters
+text = text.lower() # Convert to lowercase
+text = ' '.join([word for word in word_tokenize(text) if word not in stop_words]) # Remove stopwords
+text = ' '.join([lemmatizer.lemmatize(word) for word in word_tokenize(text)]) # Lemmatize words
+X_test = vectorizer.transform([text])
+y_pred = clf.predict(X_test)
+
+# Output as a News category
+print(y_pred)
+
+
+
+
+
+
+
+
